7cdadac63fdafd215e63cbfda450b0d944f518ec
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2022-10-13 17:18:07 -0300

    Add files via upload

---
+++ Python/Pildoras Informaticas Python/metodos-de-cadenas.py
@@ -0,0 +1,30 @@
+
+#METODOS DE CADENAS
+
+# upper() -> Convierte en mayuscula todas las letras de un string.
+
+# lower() -> Convierte en minuscula todo las letras de un string.
+
+# capitalize() -> Lo que realiza es poner el texto en parrafo es decir la primera letra en mayuscula.
+
+# count() -> Contar cuantas veces aparece una letra o una cadena de caracteres o grupo de texto en una fase.
+
+# find() -> Lo que hace es representar la posicion en la cual aparece un caracter o un grupo de caracter.
+
+# isdigit() -> devuelve un true o false (si el valor añadido es un valor numerico o no lo es.)
+
+# isalum() -> Comprueba si son alfanumericos.
+
+# isalpha() - comprueba si hay solo letras los espacios no cuentan.
+
+# split() -> separa las palabras por espacio.
+
+# strip() -> borra los espacios sobrentes del principio y el final.
+
+# replace() -> cambia una palabra o una letra por otra.
+
+# rfind() -> hace lo mismo que find pero al reves.
+
+
+# https://docs.hektorprofe.net/python/metodos-de-las-colecciones/metodos-de-las-cadenas/
+

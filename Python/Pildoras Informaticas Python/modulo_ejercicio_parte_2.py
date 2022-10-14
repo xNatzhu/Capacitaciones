294cedb112a8121dfb5c28ba6beea84c790b05f5
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2022-10-14 09:06:04 -0300

    Modulos

---
+++ Python/Pildoras Informaticas Python/modulo_ejercicio_parte_2.py
@@ -0,0 +1,17 @@
+#Ejemplo parte 2
+
+from modulos import sumar 
+
+#Esto es como javascript / el from localiza el archivo que queremos traer / y el import: importara la funcion que queramos traer a nuestro nuevo archivo.
+
+#Pero si nosotros queremos traer todo el contenido de un archivo no lo vamos a llamar uno por uno, en este caso usaremos el * esto permitira que nos traera todas las funciones o clases que se encuentren dentro del archivo externo en este caso en el archivo modulo / ejemplo:
+
+# from modulos import * 
+
+
+
+sumar(2,3) # De esta manera podremos utilizar la funcion, clase como mas deseemos.
+
+
+
+
---
+++ Python/Pildoras Informaticas Python/modulos.py
@@ -0,0 +1,22 @@
+# Que son los modulos?
+
+# Un archivo con extension py o pyc (python copilado) o archivo escrito en c para CPython, que posee su propio espacio de nombres y que puede contener variables, funciones cases e incluso otros modulos.
+
+# Sirven para organizar y reutilizar el codigo. (modularizacion  reutilizacion) -> ejemplos supongamos que tenia codigo de una aplicacion que habia creado anteriormente y ahora lo quiero re utilizar, eso es lo que haria el modulo permitir que podamos reutilizarlo nuevamente.
+
+# modularacion es crear divisiones de partes para tener una mayor organizacion. 
+
+#Ejemplo parte 1
+
+def sumar(op1, op2):
+    print("El resultado de la suma es: ", op1 + op2)
+
+
+def resta(op1, op2):
+    print("El resultado de la resta es: ", op1 - op2)
+
+def multiplicacion(op1, op2):
+    print("El resultado de la multiplicacion es: ", op1 * op2)
+
+
+#ESTO CONTINUA EN EL ARCHIVO MODULO EJERCICIO PARTE 2

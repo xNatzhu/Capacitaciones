1f3236711d4b5099f877cde74ea635b11727a147
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2022-10-19 16:58:40 -0300

    Serializacion con listas y objetos

--- Python/Pildoras Informaticas Python/serializacion.py
+++ Python/Pildoras Informaticas Python/serializacion.py
@@ -47,3 +47,61 @@ ficheroExterno2 = open("lista_nombre_serializacion", "rb") #read binary = "leera
 lista = pickle.load(ficheroExterno2) # Carga la informacion/mostrar la informacion.
 
 print(lista)
+
+
+# SERIALIZAR OBJETOS
+
+
+class Vehiculos():
+    def __init__(self, marca, modelo): #constructor
+        
+        self.marca = marca
+        self.modelo = modelo
+        self.enmarcha = False
+        self.acelera = False
+        self.frena = False
+    
+    def arrancar(self):
+        self.enmarcha = True
+
+    def acelerar(self):
+        self.acelera = True
+
+    def frenar(self):
+        self.frena = True
+        
+    def estado(self):
+        print("Marca; ", self.marca, " modelo: ", self.modelo, "en marcha: ", self.enmarcha, "¿el vehiculo se encuentra frenado?", self.frena, "¿El vehiculo acelera?", self.acelera)
+
+
+
+coche1 = Vehiculos("Marca 1", "Modelo 1")
+
+coche2 = Vehiculos("Marca 2", "Modelo 2")
+
+coche3 = Vehiculos("Marca 3", "Modelo 3")
+
+#Para no estar seriarizando uno en uno cada objeto creado que se ha inicializado, sino que los vamso a meter una lista esto permitira que podamos hacerlo de una forma mas rapida.
+
+coches= [coche1, coche2, coche3]
+ficheroExterno3 = open("Ejercicio_de_serializacion_coches", "wb")
+
+pickle.dump(coches, ficheroExterno3)
+
+ficheroExterno3.close()
+
+del (ficheroExterno3) #Una vez finalizado se puede borrar ese fichero de la memoria, porque ya se encuentra alojado en el disco duro.
+
+
+ficheroExterno3Apertura= open("Ejercicio_de_serializacion_coches","rb")
+
+#Para poder visualizar lo que hay adentro vamos a crear una variable
+
+verCochesLista = pickle.load(ficheroExterno3Apertura)
+
+#Para poder observar uno por uno los objetos que se encuentran alojado iteraremos con for
+
+for coches in verCochesLista:
+    print(coches.estado())
+
+    #Gracias al llamado metodo podemos ver la informacion.
